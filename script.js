92a20326ab6cf0962c4000c7eb0606731dbe7d29
SmarkTSAcc <u42_oskkur_lbn@technischools.com>
2024-05-21 01:41:05 +0200

    POMOCY

---
+++ script.js
@@ -0,0 +1,40 @@
+window.addEventListener('scroll', function() {
+    var header = document.querySelector('header');
+    if (window.scrollY > 0) {
+        header.style.background = 'black';
+    } else {
+        header.style.backgroundImage = 'linear-gradient(to right, black , rgb(0, 0, 137))';
+    }
+});
+document.addEventListener('DOMContentLoaded', function() {
+    var toggleSwitch = document.getElementById('theme-toggle');
+    var body = document.body;
+
+    if (localStorage.getItem('theme') === 'dark') {
+        body.classList.add('dark-mode');
+        toggleSwitch.checked = true;
+    } else {
+        body.classList.add('light-mode');
+    }
+
+    toggleSwitch.addEventListener('change', function() {
+        if (this.checked) {
+            body.classList.remove('light-mode');
+            body.classList.add('dark-mode');
+            localStorage.setItem('theme', 'dark');
+        } else {
+            body.classList.remove('dark-mode');
+            body.classList.add('light-mode');
+            localStorage.setItem('theme', 'light');
+        }
+    });
+});
+
+document.addEventListener('DOMContentLoaded', function() {
+    var scrollTrigger = document.getElementById('scrollTrigger');
+    var targetSection = document.getElementById('formularz');
+
+    scrollTrigger.addEventListener('click', function() {
+        targetSection.scrollIntoView({ behavior: 'smooth' });
+    });
+});
